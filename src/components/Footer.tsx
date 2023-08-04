dcd286f77e0ce54b52a4ce647ad975a60af48b9f
Abdul Moqueet <moqueetabdul@gmail.com>
2023-08-04 10:58:32 +0530

    ui bug fixing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-blackish text-gray-500 text-center py-4 pb-16 md:pb-4">
-      Copyright © Logo <br /> All Rights Reserved 2023.
+      Copyright © Sick <br /> All Rights Reserved 2023.
     </div>
   );
 };
--- src/components/HeaderMain.tsx
+++ src/components/HeaderMain.tsx
@@ -10,7 +10,7 @@ const HeaderMain = () => {
     <div className="border-b border-gray-200 py-6">
       <div className="container sm:flex justify-between items-center">
         <div className="font-bold text-4xl text-center pb-4 sm:pb-0 text-blackish">
-          Logo
+          Sick
         </div>
 
         <div className="w-full sm:w-[300px] md:w-[70%] relative">
